40f6c23279d4f7396c57d072e05f6f823453a22b
isha-andrade <isha@konradinvestments.com>
2025-08-07 18:15:43 +0400

    post routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
 import {clerkMiddleware} from "@clerk/express";
-import userRoutes from "./routes/user.route.js"
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 const app = express();
 
@@ -15,6 +16,13 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("Hello from server (～￣▽￣)～"))
 
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.use((err, req, res) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({error: err.message || "Internal server error"});
+});
+
 
 const startServer = async () => {
   try {
