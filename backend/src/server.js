adf631576d6f4a47b19e298f604eae50d8af8d16
isha-andrade <isha@konradinvestments.com>
2025-08-07 18:21:52 +0400

    tiny fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => res.send("Hello from server (～￣▽￣)～"))
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error("Unhandled error:", err);
   res.status(500).json({error: err.message || "Internal server error"});
 });
